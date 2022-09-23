c2b47c842f24ab1d867652847d230949543d8f5c
JoyeChen-Got <105471886+JoyeChen-Got@users.noreply.github.com>
2022-09-23 12:27:45 +0800

    Add files via upload

---
+++ file/LeanCloud评论.js
@@ -0,0 +1,9 @@
+<script src='//unpkg.com/valine/dist/Valine.min.js'></script>
+<div id="vcomments"></div>
+<script>
+    new Valine({
+        el: '#vcomments',
+        appId: 'TIoXCeMrnLw44wnsAWbxzkqt-MdYXbMMI',
+        appKey: 'l6oXDBfNCt1v4gNC9ryrbwgR'
+    })
+</script>
